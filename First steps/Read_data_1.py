1845dc65656b9e73601ba23689282b2987faa5c3
encent <nickandroid1@gmail.com>
2015-03-11 22:51:59 +0300

    Same genes problem 3

--- First steps/Read_data_1.py
+++ First steps/Read_data_1.py
@@ -6,7 +6,6 @@ list.clear()
 for token in l:
     list.append(token.split("\t"))
 # del list[0]
-d = {}
 a = set()
 for i in range(0, len(list), 1):
     if list[i][3] == "NA":
@@ -18,20 +17,30 @@ for i in range(0, len(list), 1):
         a.add(list[i][0])
 a.remove("GeneSymbol")
 b = set()
-for i in range(1, len(list), 1):
-    if len(list[i]) != 0:
-        if not(list[i][0] in b):
-            d[list[i][0]] = list[i][1:]
-            b.add(list[i][0])
-        else:
-            buf = d[list[i][0]]
-            form = []
-            for j in range(1, min(len(list[i]), len(buf)), 1):
-                form.append(str(float(list[i][j]) + float(buf[j])))
-            d[list[i][0]] = form
+sog = []
 for elem in a:
-    if len(d[elem]) != 0:
-        clear_data.write(elem + "\t" + "\t".join(d[elem]) + "\n")
+    sog.append(elem)
+d = dict.fromkeys(sog, [])
+c = dict.fromkeys(sog, 0)
+for gene in list:
+    if len(gene) != 0:
+        if gene[0] != "GeneSymbol":
+            if len(d[gene[0]]) == 0:
+                d[gene[0]] = gene[1:]
+                c[gene[0]] += 1
+            else:
+                buf = d.get(gene[0])
+                for i in range(0, len(buf)):
+                    buf[i] = str(float(buf[i]) + float(gene[i + 1]))
+                d[gene[0]] = buf
+                c[gene[0]] += 1
+
+for elem in d:
+    for i in range(0, len(d.get(str(elem)))):
+        d.get(str(elem))[i] = str(round(float(d.get(str(elem))[i]) / c.get(str(elem)), 4))
+
+for elem in d:
+    clear_data.write(str(elem) + "\t" + "\t".join(d.get(str(elem))) + "\n" * 2)
+
 clear_data.close()
 data.close()
-
